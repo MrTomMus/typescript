0fcd17df9d753b2b1b1d9f9c8d89d273620651f9
MrTomMus <tommusshow@gmail.com>
2023-07-10 17:27:17 +0300

    feat: add typescript in project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Card from './components/Card';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Card width={'400px'} height={'400px'}/>
+    </div>
+  );
+}
+
+export default App;
